98bcff6a32f02b8b53696906741b257898a587c1
Nguyen Minh Duong <nguyen.minh.duong@framgia.com>
2017-06-08 11:41:28 +0700

    Request leave logic

--- app/src/main/java/com/framgia/wsm/data/model/Request.java
+++ app/src/main/java/com/framgia/wsm/data/model/Request.java
@@ -34,14 +34,26 @@ public class Request extends BaseModel implements Parcelable {
     @SerializedName("project")
     private String mProject;
     @Expose
-    @SerializedName("from")
+    @SerializedName("from_time")
     private String mFromTime;
     @Expose
-    @SerializedName("to")
+    @SerializedName("end_time")
     private String mToTime;
+    @Expose
+    @SerializedName("checkin_time")
+    private String mCheckinTime;
+    @Expose
+    @SerializedName("checkout_time")
+    private String mCheckoutTime;
     @Expose
     @SerializedName("reason")
     private String mReason;
+    @Expose
+    @SerializedName("status")
+    private int mStatus;
+    @Expose
+    @SerializedName("compensation")
+    private Compensation mCompensation;
 
     protected Request(Parcel in) {
         mPositionLeaveType = in.readInt();
@@ -50,6 +62,7 @@ public class Request extends BaseModel implements Parcelable {
         mFromTime = in.readString();
         mToTime = in.readString();
         mReason = in.readString();
+        mStatus = in.readInt();
     }
 
     public Request() {
@@ -68,6 +81,7 @@ public class Request extends BaseModel implements Parcelable {
         dest.writeString(mFromTime);
         dest.writeString(mToTime);
         dest.writeString(mReason);
+        dest.writeInt(mStatus);
     }
 
     public int getPositionLeaveType() {
@@ -132,5 +146,104 @@ public class Request extends BaseModel implements Parcelable {
 
     public void setReason(String reason) {
         mReason = reason;
+    }
+
+    public String getCheckinTime() {
+        return mCheckinTime;
+    }
+
+    public void setCheckinTime(String checkinTime) {
+        mCheckinTime = checkinTime;
+    }
+
+    public String getCheckoutTime() {
+        return mCheckoutTime;
+    }
+
+    public void setCheckoutTime(String checkoutTime) {
+        mCheckoutTime = checkoutTime;
+    }
+
+    public int getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(int status) {
+        mStatus = status;
+    }
+
+    public Compensation getCompensation() {
+        return mCompensation;
+    }
+
+    public void setCompensation(Compensation compensation) {
+        mCompensation = compensation;
+    }
+
+    public static class Compensation implements Parcelable {
+        public static final Creator<Compensation> CREATOR = new Creator<Compensation>() {
+            @Override
+            public Compensation createFromParcel(Parcel in) {
+                return new Compensation(in);
+            }
+
+            @Override
+            public Compensation[] newArray(int size) {
+                return new Compensation[size];
+            }
+        };
+        @SerializedName("compensation_from")
+        private String mFromTime;
+        @Expose
+        @SerializedName("compensation_to")
+        private String mToTime;
+        @Expose
+        @SerializedName("status")
+        private int mStatus;
+
+        protected Compensation(Parcel in) {
+            mFromTime = in.readString();
+            mToTime = in.readString();
+            mStatus = in.readInt();
+        }
+
+        public Compensation() {
+        }
+
+        public String getFromTime() {
+            return mFromTime;
+        }
+
+        public void setFromTime(String fromTime) {
+            mFromTime = fromTime;
+        }
+
+        public String getToTime() {
+            return mToTime;
+        }
+
+        public void setToTime(String toTime) {
+            mToTime = toTime;
+        }
+
+        public int getStatus() {
+            return mStatus;
+        }
+
+        public void setStatus(int status) {
+            mStatus = status;
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(mFromTime);
+            dest.writeString(mToTime);
+            dest.writeInt(mStatus);
+        }
     }
 }
