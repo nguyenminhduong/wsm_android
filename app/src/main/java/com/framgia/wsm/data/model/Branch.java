e4981069c47210e73deee3eb9c3a2ad14dd2e72f
Tran Minh Tri <tranminhtri911@gmail.com>
2017-06-12 10:31:39 +0700

    [Sprint 3] Feature logic request overtime #3

--- app/src/main/java/com/framgia/wsm/data/model/Branch.java
+++ app/src/main/java/com/framgia/wsm/data/model/Branch.java
@@ -1,10 +1,24 @@
 package com.framgia.wsm.data.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by tri on 31/05/2017.
  */
 
-public class Branch extends BaseModel {
+public class Branch extends BaseModel implements Parcelable {
+    public static final Creator<Branch> CREATOR = new Creator<Branch>() {
+        @Override
+        public Branch createFromParcel(Parcel source) {
+            return new Branch(source);
+        }
+
+        @Override
+        public Branch[] newArray(int size) {
+            return new Branch[size];
+        }
+    };
     private String mBranchId;
     private String mBranchName;
 
@@ -14,6 +28,11 @@ public class Branch extends BaseModel {
     public Branch(String branchId, String branchName) {
         mBranchId = branchId;
         mBranchName = branchName;
+    }
+
+    protected Branch(Parcel in) {
+        this.mBranchId = in.readString();
+        this.mBranchName = in.readString();
     }
 
     public String getBranchId() {
@@ -30,5 +49,16 @@ public class Branch extends BaseModel {
 
     public void setBranchName(String branchName) {
         mBranchName = branchName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mBranchId);
+        dest.writeString(this.mBranchName);
     }
 }
--- app/src/main/java/com/framgia/wsm/screen/requestovertime/confirmovertime/ConfirmOvertimeViewModel.java
+++ app/src/main/java/com/framgia/wsm/screen/requestovertime/confirmovertime/ConfirmOvertimeViewModel.java
@@ -1,6 +1,8 @@
 package com.framgia.wsm.screen.requestovertime.confirmovertime;
 
+import android.view.View;
 import com.framgia.wsm.data.model.Request;
+import com.framgia.wsm.utils.navigator.Navigator;
 
 /**
  * Exposes the data to be used in the ConfirmOvertime screen.
@@ -10,12 +12,14 @@ public class ConfirmOvertimeViewModel implements ConfirmOvertimeContract.ViewMod
 
     private ConfirmOvertimeContract.Presenter mPresenter;
     private Request mRequest;
+    private Navigator mNavigator;
 
-    public ConfirmOvertimeViewModel(ConfirmOvertimeContract.Presenter presenter,
-            Request request) {
+    public ConfirmOvertimeViewModel(ConfirmOvertimeContract.Presenter presenter, Request request,
+            Navigator navigator) {
         mPresenter = presenter;
         mPresenter.setViewModel(this);
         mRequest = request;
+        mNavigator = navigator;
     }
 
     @Override
@@ -30,5 +34,9 @@ public class ConfirmOvertimeViewModel implements ConfirmOvertimeContract.ViewMod
 
     public Request getRequest() {
         return mRequest;
+    }
+
+    public void onCickArrowBack(View view) {
+        mNavigator.finishActivity();
     }
 }
