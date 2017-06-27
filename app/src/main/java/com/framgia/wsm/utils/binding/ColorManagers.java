4fa2d246fee74c32e81a6fdb5a28c186f8872323
Tran Minh Tri <tranminhtri911@gmail.com>
2017-06-27 13:51:46 +0700

    [Sprint 6] Feature profile ui #2

---
+++ app/src/main/java/com/framgia/wsm/utils/binding/ColorManagers.java
@@ -0,0 +1,45 @@
+package com.framgia.wsm.utils.binding;
+
+import android.graphics.Color;
+
+/**
+ * Created by tri on 27/06/2017.
+ */
+
+public class ColorManagers {
+
+    public ColorManagers() {
+    }
+
+    public static int getColorRed() {
+        return Color.RED;
+    }
+
+    public static int getColorBlue() {
+        return Color.BLUE;
+    }
+
+    public static int getColorGreen() {
+        return Color.GREEN;
+    }
+
+    public static int getColorYellow() {
+        return Color.YELLOW;
+    }
+
+    public static int getColorBlack() {
+        return Color.BLACK;
+    }
+
+    public static int getColorCyan() {
+        return Color.CYAN;
+    }
+
+    public static int getColorDkgray() {
+        return Color.DKGRAY;
+    }
+
+    public static int getColorMagentay() {
+        return Color.MAGENTA;
+    }
+}
