7ca1c85ed219e68c89e93134451c0064ab93279c
DaoLQ <le.quang.dao@framgia.com>
2017-07-14 11:23:16 +0700

    Config CI Review

--- scripts/chatwork.rb
+++ scripts/chatwork.rb
@@ -1,7 +1,7 @@
-unless ARGV[0].empty?
+unless ARGV[1].empty?
 	require "chatwork"
 
-	# Create message
-	ChatWork.api_key = "8f247d4a711fb2326012a8ad47243db3"
-	ChatWork::Message.create(room_id: 75951589, body: "[info][To:2135825] [To:2198020] [To:2198033] [To:1979687] [To:2423114] \n\n CI Build Success! \n\n Please Check! \n Link: #{ARGV[0]} [/info]")
+  # Create message
+  ChatWork.api_key = "8f247d4a711fb2326012a8ad47243db3"
+  ChatWork::Message.create(room_id: "#{ARGV[0]}", body: "[info](ok2)[hr]Project: #{ARGV[4]} \n\nOwner: #{ARGV[3]} \n\nPlease Check! \nLink : #{ARGV[1]}[hr]#{ARGV[2]}[/info]")
 end
